9c1250f03879f31a72540082452df193c73158d1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-29 21:43:12 -0700

    MAIN.lua [V1]
    Version 1 of the main Lua script for this project.

---
+++ Interface/MAIN.lua
@@ -0,0 +1,43 @@
+-- Start of script
+-- The main interface for Green Star OS
+-- Written in MoonScript 0.5.0, compiled manually to Lua ??
+
+-- About page
+local function about = -> print "About Green Star OS\nInterface for Green Star OS\nStar and moon edition\nVersion 0.0.1 - 2022, Tuesday, March 29th at 9:38 pm\nLicensed under the GNU general public license V3.0\n[Click/tap here toview the license file](/LICENSE-GPL.txt)"
+function about = function() end
+about()
+
+-- Start of the interface program
+local main
+print "Welcome to Green Star OS."
+-- "Boot" manager
+print "Choose a mode: \nGreen Star OS 1 (Mimicks Red Star OS 1)\nGreen Star OS 2 (Mimicks Red Star OS 2)\nGreen Star OS 3 (Mimicks Red Star OS 3)"
+
+-- Green Star OS 1
+local function mode_gs1 = -> -- Currently empty
+function mode_gs1 = function() end
+mode_gs1()
+
+-- Green Star OS 2
+local function mode_gs2 = -> -- Currently empty
+function mode_gs2 = function() end
+mode_gs2()
+
+-- Green Star OS 3
+local function mode_gs3 = -> -- Currently empty
+function mode_gs3 = function() end
+mode_gs3()
+
+-- I am not sure what else to do right now
+
+-- File info
+-- File type: MoonScript source file (*.moon)
+-- File version: 1 (2022, Tuesday, March 29th at 9:38 pm)
+-- Line count (including blank lines and compiler line): 32
+
+-- File info
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Tuesday, March 29th at 9:43 pm)
+-- Line count (including blank lines and compiler line): 44
+
+-- End of script
