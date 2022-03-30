3b92ca242bdeeadb33893a19bf32b8f82b72ad12
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-29 21:47:03 -0700

    .deps.lua [V1] [Dependencies]
    Version 1 of the dependencies file for this project.

---
+++ .deps.lua
@@ -0,0 +1,11 @@
+-- Start of file
+-- Dependencies for Green Star OS
+-- MoonScript V0.5.0 or later
+-- Lua 5.4 or later
+print "Dependencies for Green Star OS\nMoonScript V0.5.0 or later\nLua 5.4 or later"
+break
+-- File info
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Tuesday, March 29th at 9:47 pm)
+-- Line count (including blank lines and compiler line): 12
+-- End of file
