d9abc03a4282675bb7b4b3cb62c25df5bf455106
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-31 16:23:06 -0700

    MAIN_V2.0.lua
    An archive of the 2nd version of the main script file for this project.

---
+++ Interface/!OldVersions/MAIN/Lua/1/1-100/MAIN_V2.0.lua
@@ -0,0 +1,50 @@
+-- Start of script
+-- The main interface for Green Star OS
+-- Written in MoonScript 0.5.0, compiled manually to Lua 5.4
+
+-- About page
+local function about = -> print "About Green Star OS\nInterface for Green Star OS\nStar and moon edition\nVersion 0.0.1 - 2022, Tuesday, March 29th at 9:38 pm\nLicensed under the GNU general public license V3.0\n[Click/tap here toview the license file](/LICENSE-GPL.txt)"
+function about = function() end
+about()
+
+-- Start of the interface program
+local main
+print "Welcome to Green Star OS."
+-- "Boot" manager
+print "Choose a mode: \nGreen Star OS 1 (Mimicks Red Star OS 1)\nGreen Star OS 2 (Mimicks Red Star OS 2)\nGreen Star OS 3 (Mimicks Red Star OS 3)\nGreen Star OS 4 (Mimicks Red Star OS 4)"
+
+-- Green Star OS 1
+local function mode_gs1 = -> -- Currently empty
+function mode_gs1 = function() end
+mode_gs1()
+
+-- Green Star OS 2
+local function mode_gs2 = -> -- Currently empty
+function mode_gs2 = function() end
+mode_gs2()
+
+-- Green Star OS 3
+local function mode_gs3 = -> -- Currently empty
+function mode_gs3 = function() end
+mode_gs3()
+
+-- I was made aware of Red Star OS 4 yesterday (2022, Wednesday, March 30th) and decided to give this script a little update
+
+-- Green Star OS 4
+local function mode_gs4 = -> -- Currently empty
+function mode_gs4 = function() end
+mode_gs3()
+
+-- I am not sure what else to do right now
+
+-- File info
+-- File type: MoonScript source file (*.moon)
+-- File version: 2 (2022, Thursday, March 31st at 12:00 am)
+-- Line count (including blank lines and compiler line): 40
+
+-- File info
+-- File type: Lua source file (*.lua)
+-- File version: 2 (2022, Thursday, March 31st at 12:02 am)
+-- Line count (including blank lines and compiler line): 51
+
+-- End of script
