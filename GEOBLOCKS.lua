2feedf5edc55474a536642d9fd93c101929d1447
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-03 17:23:01 -0700

    GEOBLOCKS.lua [V1]
    Version 1 of the GEOBLOCKS Lua file for this project.

---
+++ GEOBLOCKS.lua
@@ -0,0 +1,22 @@
+-- Start of script
+-- GEOBLOCKS.lua
+-- Written in: Lua 5.4
+-- This is a list of countries this project is Geo-blocked in/Likely to be Geo-blocked in
+print "Geoblocks\nThis is a list of countries this project is Geo-blocked in/Likely to be Geo-blocked in"
+local function geob1 = -> print "North Korea"
+function mode_geob1 = function() end
+mode_geob1()
+local function geob2 = -> print "South Korea"
+function mode_goeb2 = function() end
+mode_geob2()
+local function geob3 = -> print "China"
+function mode_goeb3 = function() end
+mode_geob3()
+geob1()
+geob2()
+geob3()
+-- File info
+-- File type: Lua source file (*.lua)
+-- File version: 1 (2022, Sunday, April 3rd at 5:23 pm)
+-- Line count (including blank lines and compiler line): 23
+-- End of script
